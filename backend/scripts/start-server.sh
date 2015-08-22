8a556fa9c5688482fbef06e7f7ed9a3d5b636b79
Tim Dumol <tim@timdumol.com>
2015-08-22 22:24:25 +0800

    Install Redis.

--- backend/scripts/start-server.sh
+++ backend/scripts/start-server.sh
@@ -2,4 +2,4 @@
 set -e
 cd /var/www/angular-momentum/backend
 source /var/www/angular-momentum/virtualenv/bin/activate
-gunicorn --paste development.ini -b:8000 --chdir .
+exec gunicorn --paste development.ini -b127.0.0.1:8000 --chdir .
